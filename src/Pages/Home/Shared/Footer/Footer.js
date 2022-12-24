e283195c3b66644982dd3a658644be44994c0381
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-24 15:48:29 +0600

    added footer

--- src/Pages/Home/Shared/Footer/Footer.js
+++ src/Pages/Home/Shared/Footer/Footer.js
@@ -1,8 +1,32 @@
 const Footer = () => {
   return (
-    <div>
-      <h1>This is footer</h1>
-    </div>
+    <section className="footer-bg">
+      <footer className="footer lg:justify-around justify-start al p-10">
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+      <div className="text-center pb-6">
+        <p>Copyright © 2022 - All right reserved by Mizba Uddin Tareq</p>
+      </div>
+    </section>
   );
 };
 
