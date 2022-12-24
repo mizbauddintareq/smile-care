d233592ceb50cfb3daec10e05f7e49a845c78d04
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-24 15:54:51 +0600

    added footer

--- src/Pages/Home/Shared/Footer/Footer.js
+++ src/Pages/Home/Shared/Footer/Footer.js
@@ -1,26 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <section className="footer-bg">
       <footer className="footer lg:justify-around justify-start al p-10">
         <div>
-          <span className="footer-title">Services</span>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <span className="footer-title">SERVICES</span>
+          <Link className="link link-hover">Emergency Checkup</Link>
+          <Link className="link link-hover">Monthly Checkup</Link>
+          <Link className="link link-hover">Weekly Checkup</Link>
+          <Link className="link link-hover">Deep Checkup</Link>
         </div>
         <div>
-          <span className="footer-title">Company</span>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <span className="footer-title">ORAL HEALTH</span>
+          <Link className="link link-hover">Fluoride Treatment</Link>
+          <Link className="link link-hover">Cavity Filling</Link>
+          <Link className="link link-hover">Teath Whitening</Link>
         </div>
         <div>
-          <span className="footer-title">Legal</span>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <span className="footer-title">Contact Info</span>
+          <p>Kolatoli, Cox's Bazar, Bangladesh</p>
+          <p>mizbauddintareq@gmail.com</p>
+          <p>+880 1580541540</p>
         </div>
       </footer>
       <div className="text-center pb-6">
